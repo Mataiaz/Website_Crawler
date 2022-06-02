85fa36d2e9711154c2178d93ab81f0494bc1e162
Mataiaz <mathias344@live.no>
2022-06-02 20:03:45 +0200

    deleted empty file

--- src/main/java/com/example/Input.java
+++ src/main/java/com/example/Input.java
@@ -10,7 +10,7 @@ import org.apache.poi.ss.usermodel.Cell;
 public class Input {
     public static void main(int rowNbr, String fileLocation) throws IOException {
         try {
-            XSSFWorkbook wb = new XSSFWorkbook(fileLocation);
+        XSSFWorkbook wb = new XSSFWorkbook(fileLocation);
         XSSFSheet sheet = wb.getSheetAt(0);
         // evaluating cell type
         Row row = sheet.getRow(rowNbr);
